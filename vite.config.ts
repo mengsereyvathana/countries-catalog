3af654a50f307e7fec4ce460aa1ec2a394a19ef9
Sereyvathana <mengvatana101@gmail.com>
2024-01-25 16:37:01 +0700

    config base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/countries-catalog',
+  base: '/countries-catalog/',
   plugins: [
     vue(),
   ],
