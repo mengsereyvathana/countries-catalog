00d26d634da58a77fd240c4aad2060168b03f583
Sereyvathana <mengvatana101@gmail.com>
2024-01-25 15:00:47 +0700

    add tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="text-red-500 underline">
+    Home page
+  </div>
 </template>
